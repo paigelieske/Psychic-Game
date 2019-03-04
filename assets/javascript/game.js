89fb5f7f6b51f819cacb98c960ab1939e3fbf812
Paige Lieske <paigeproctor@comcast.net>
2019-03-04 13:49:25 -0500

    added code to restrict guesses to letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,35 +13,39 @@ var guessesLeftText = document.getElementById("guessesLeft-text");
 var guessesTriedText = document.getElementById("guessesTried-text");
 
 document.onkeyup = function() {
-    var userGuess = event.key;
-    userGuess = userGuess.toLowerCase(event.key);
-    guessesTried.push(userGuess);
-    console.log(computerGuess);
-
-    if (userGuess === computerGuess) {
-        wins++;
-        guessesLeft = 9;
-        guessesTried = [];
-        console.clear();
-        alert("Your third eye is strong.");
-        computerGuess = alphaArray[Math.floor(Math.random() * alphaArray.length)];
-    }
-    else if (userGuess != computerGuess) {
-        guessesLeft--;
-        if (guessesLeft === 0) {
-            losses++;
-            guessesLeft = 9;
-            guessesTried = [];
-            console.clear();
-            alert("You must not be one with the force.");
-            computerGuess = alphaArray[Math.floor(Math.random() * alphaArray.length)];
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var check = alphaArray.includes(userGuess);
+        
+        if (check === false) {
+            alert("That's not a letter....tsk tsk.");
         }
-    }
+        else if (check === true) {
+            guessesTried.push(userGuess);
+            console.log(computerGuess);    
+
+            if (userGuess === computerGuess) {
+                wins++;
+                guessesLeft = 9;
+                guessesTried = [];
+                console.clear();
+                alert("Your third eye is strong.");
+                computerGuess = alphaArray[Math.floor(Math.random() * alphaArray.length)];
+            }
+            else if (userGuess != computerGuess) {
+                guessesLeft--;
+                if (guessesLeft === 0) {
+                    losses++;
+                    guessesLeft = 9;
+                    guessesTried = [];
+                    console.clear();
+                    alert("You must not be one with the force.");
+                    computerGuess = alphaArray[Math.floor(Math.random() * alphaArray.length)];
+                }
+            }
+        }
     
-    //don't allow duplicate userGuess
-    
-    // else if statement to alert the user when they enter a non-letter key (ex: space, esc, #, etc.)
-    
+    // need to add code restricting duplicate userGuess
+      
     winsText.textContent = "Wins: " + wins;
     lossesText.textContent = "Losses: " + losses;
     guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
